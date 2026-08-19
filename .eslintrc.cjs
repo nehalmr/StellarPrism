fc2088d17331679649c58330b27563732b3e4d28
nehalrane9.7 <nehalrane9.7@gmail.com>
2026-08-19 22:43:40 +0530

    chore(ci): add ESLint config and CI workflow; release v1.0.0

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    // project-specific rule overrides can go here
+  },
+};
